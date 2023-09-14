79cb4df9b07e80e0d23330048a78552f56457193
chistym17 <mchisty31@gmail.com>
2023-09-14 11:49:47 +0600

    updated

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,19 @@
-
-from django.shortcuts import render
-from .models import Project
-from django.db.models import Avg
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Project, Rating
+from .forms import RatingForm
 
 def project_list(request):
+    # if request.method == 'POST':
+    #     project_id = request.POST.get('project_id')
+    #     project = get_object_or_404(Project, pk=project_id)
+    #     form = RatingForm(request.POST)
+    #     if form.is_valid():
+    #         rating = form.save(commit=False)
+    #         rating.project = project
+    #         rating.save()
+    #         return redirect('projectlist')
+
     projects = Project.objects.all()
-    return render(request, 'show_projects.html', {'projects': projects})
-
-
-
+    form = RatingForm()
+    
+    return render(request, 'show_projects.html', {'projects': projects, 'form': form})
